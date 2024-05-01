01f115bcb8ab93dad7e849a4553d2b923814177f
Parth Jindal <pmjindal@gmail.com>
2024-05-02 04:50:00 +0530

    update

--- deploy_gpt.py
+++ deploy_gpt.py
@@ -9,14 +9,16 @@ if "openai_model" not in st.session_state:
     st.session_state["openai_model"] = "gpt-3.5-turbo"
 
 if "messages" not in st.session_state:
-    st.session_state.messages = []
+    st.session_state.messages = [
+        {"role": "system", "content": "You are a legal AI assistant that will be used by legal and non-legal professionals for any kind of legal, compliance and regulatory questions. Help them with their queries."}]
+
 
 # Define default questions and create buttons for them
 default_questions = [
     "How to register my startup in US Delaware C Corp?",
     "How to sue a competitor company for using my trademarked logo?",
     "Hit and Run case fall into which section of IPC?",
-    
+
 ]
 
 default_ques = False
@@ -26,6 +28,12 @@ for question in default_questions:
         default_ques = True
         if "messages" not in st.session_state:
             st.session_state.messages = []
+
+        if "messages" not in st.session_state:
+            st.session_state.messages = [
+                {"role": "system", "content": "You are a legal AI assistant that will be used by legal and non-legal professionals for any kind of legal, compliance and regulatory questions. Help them with their queries."}]
+
+
 
         st.session_state.messages.append({"role": "user", "content": question})
         with st.chat_message("user"):
@@ -41,7 +49,8 @@ for question in default_questions:
                 stream=True,
             )
             response = st.write_stream(stream)
-        st.session_state.messages.append({"role": "assistant", "content": response})
+        st.session_state.messages.append(
+            {"role": "assistant", "content": response})
 
 if not default_ques:
     for message in st.session_state.messages:
@@ -63,4 +72,5 @@ if prompt := st.chat_input("What is up?"):
             stream=True,
         )
         response = st.write_stream(stream)
-    st.session_state.messages.append({"role": "assistant", "content": response})
+    st.session_state.messages.append(
+        {"role": "assistant", "content": response})
